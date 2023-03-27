f737672fcbc2e39e39260d465d75994b7d05aa83
Anshu Sinha <anshujuly2@gmail.com>
2023-03-27 19:52:58 +0530

    Add a code to draw a square with the turtle object

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+"""
+from turtle module, Turtle and Screen classes are imported
+"""
 from turtle import Turtle, Screen
 
+"""tim object is created"""
 tim = Turtle()
+"""a turtle shape is provided """
 tim.shape("turtle")
+"""tim color is set to blue"""
 tim.color("blue")
 
-
+"""function to create a square"""
 def timMakeSquare():
     tim.forward(100)
     tim.right(90)
@@ -14,8 +20,10 @@ def timMakeSquare():
     tim.right(90)
     tim.forward(100)
 
-
+"""function is called"""
 timMakeSquare()
 
+"""screen object is created"""
 screen = Screen()
+"""screen will be exited upon a click"""
 screen.exitonclick()
