532baf69e11238c8b84537b52601476ea9d4c3ab
Anshu Sinha <anshujuly2@gmail.com>
2023-03-27 19:55:04 +0530

    Add a code to draw different size polygons using the turtle object

--- main.py
+++ main.py
@@ -23,15 +23,29 @@ tim.color("blue")
 # """function is called"""
 # timMakeSquare()
 
-"""function to create a dashed line"""
-def timDashedLine():
-    tim.pendown()
-    tim.forward(10)
-    tim.penup()
-    tim.forward(10)
+# """function to create a dashed line"""
+# def timDashedLine():
+#     tim.pendown()
+#     tim.forward(10)
+#     tim.penup()
+#     tim.forward(10)
+#
+# for i in range(10):
+#     timDashedLine()
 
-for i in range(10):
-    timDashedLine()
+"""function to create different sizes i.e from triangle to decagon"""
+def timMakePolygon(angle, color):
+    tim.color(color)
+    tim.forward(100)
+    tim.right(angle)
+
+colors = ["#ade8f4", "#90e0ef", "#48cae4", "#00b4d8", "#0096c7", "#0077b6", "#023e8a", "#03045e"]
+count = 0
+for i in range(3, 11):
+    color = colors[count]
+    for j in range(0, i):
+        timMakePolygon(360/i, color)
+    count += 1
 
 """screen object is created"""
 screen = Screen()
