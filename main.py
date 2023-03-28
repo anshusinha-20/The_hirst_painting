633df9ca4604e64478dec752ee884ea4bb69ce3a
Anshu Sinha <anshujuly2@gmail.com>
2023-03-28 10:34:14 +0530

    Modify timCreateHirstPainting function

--- main.py
+++ main.py
@@ -121,78 +121,88 @@ screen.setup(1000, 800)
 
 ##### ----- #####
 
-"""function to draw a spirograph"""
-def timDrawSpirograph(x, y):
-    # tim's configurations
-    turtle.colormode(255)
+# """function to draw a spirograph"""
+# def timDrawSpirograph(x, y):
+#     # tim's configurations
+#     turtle.colormode(255)
+#
+#     def generateRandomColor():
+#         red = random.randint(0, 255)
+#         blue = random.randint(0, 255)
+#         green = random.randint(0, 255)
+#         color = (red, green, blue)
+#         return color
+#
+#     for i in range(180):
+#         tim.color(generateRandomColor())
+#         tim.circle(100)
+#         tim.setheading(tim.heading() + 2)
+#
+#     tim.hideturtle()
+#     screen.exitonclick()
+#
+# """function called on screen click"""
+# screen.onscreenclick(timDrawSpirograph, 1)
+# turtle.mainloop()
 
-    def generateRandomColor():
-        red = random.randint(0, 255)
-        blue = random.randint(0, 255)
-        green = random.randint(0, 255)
-        color = (red, green, blue)
-        return color
-
-    for i in range(180):
-        tim.color(generateRandomColor())
-        tim.circle(100)
-        tim.setheading(tim.heading() + 2)
-
-    tim.hideturtle()
-    screen.exitonclick()
-
-"""function called on screen click"""
-screen.onscreenclick(timDrawSpirograph, 1)
-turtle.mainloop()
+##### ----- #####
 
 ##### ----- #####
 
 # ##### day18: The Hirst painting project
-# """
-# imports colorgram module
+# """function to create hirst painting"""
+# def timCreateHirstPainting(x, y):
+#     """
+#     imports colorgram module
 #
-# The colorgram module is a third-party library in Python that can be used
-# to extract colors from an image. It works by analyzing an image file and
-# creating a list of the dominant colors present in the image, along with
-# their RGB values. This can be useful in a variety of applications, such
-# as generating color palettes or analyzing the color composition of an
-# image. In the code provided, the colorgram module is used to extract a
-# list of 50 dominant colors from the image1.jpg file, which are then used
-# to randomly color the dots in the turtle graphic.
-# """
-# import colorgram
+#     The colorgram module is a third-party library in Python that can be used
+#     to extract colors from an image. It works by analyzing an image file and
+#     creating a list of the dominant colors present in the image, along with
+#     their RGB values. This can be useful in a variety of applications, such
+#     as generating color palettes or analyzing the color composition of an
+#     image. In the code provided, the colorgram module is used to extract a
+#     list of 50 dominant colors from the image1.jpg file, which are then used
+#     to randomly color the dots in the turtle graphic.
+#     """
+#     import colorgram
 #
-# """extracts and stores 50 colors from the provided image"""
-# colors = colorgram.extract("image1.jpg", 50)
+#     """extracts and stores 50 colors from the provided image"""
+#     colors = colorgram.extract("image1.jpg", 50)
 #
-# """rgbColors list"""
-# colorList = []
+#     """rgbColors list"""
+#     colorList = []
 #
-# """loop runs and appends all the colors as rgb value in rgbColors list"""
-# for color in colors:
-#     red = color.rgb.r
-#     green = color.rgb.g
-#     blue = color.rgb.b
-#     rgbColor = (red, green, blue)
-#     colorList.append(rgbColor)
+#     """loop runs and appends all the colors as rgb value in rgbColors list"""
+#     for color in colors:
+#         red = color.rgb.r
+#         green = color.rgb.g
+#         blue = color.rgb.b
+#         rgbColor = (red, green, blue)
+#         colorList.append(rgbColor)
 #
-# # tim's configurations
-# turtle.colormode(255)
-# tim.penup()
-# tim.speed("fastest")
-# tim.goto(-200, -200)
+#     # tim's configurations
+#     turtle.colormode(255)
+#     tim.penup()
+#     tim.goto(-200, -200)
 #
-# dotsNumber = 100
+#     dotsNumber = 100
 #
-# for dot in range(1, dotsNumber + 1):
-#     tim.dot(20, random.choice(colorList))
-#     tim.forward(40)
-#
-#     if dot % 10 == 0:
-#         tim.setheading(90)
+#     for dot in range(1, dotsNumber + 1):
+#         tim.dot(20, random.choice(colorList))
 #         tim.forward(40)
-#         tim.setheading(180)
-#         tim.forward(400)
-#         tim.setheading(0)
 #
-# tim.hideturtle()
+#         if dot % 10 == 0:
+#             tim.setheading(90)
+#             tim.forward(40)
+#             tim.setheading(180)
+#             tim.forward(400)
+#             tim.setheading(0)
+#
+#     tim.hideturtle()
+#     screen.exitonclick()
+#
+# """function called on screen click"""
+# screen.onscreenclick(timCreateHirstPainting, 1)
+# turtle.mainloop()
+
+##### ----- #####
