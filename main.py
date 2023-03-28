b1f4b7b1d02abb7c4d5d5e3e7c164623aa4560da
Anshu Sinha <anshujuly2@gmail.com>
2023-03-28 09:30:13 +0530

    Add a code to draw hirst painting using the turtle object

--- main.py
+++ main.py
@@ -9,10 +9,8 @@ import random
 
 """tim object is created"""
 tim = Turtle()
-# """a turtle shape is provided """
-# tim.shape("turtle")
-# """tim color is set to blue"""
-# tim.color("blue")
+"""a turtle shape is provided """
+tim.shape("turtle")
 
 """screen object is created"""
 screen = Screen()
@@ -103,6 +101,55 @@ screen.setup(1000, 800)
 #
 # tim.hideturtle()
 
+# ##### day18: The Hirst painting project
+# """
+# imports colorgram module
+#
+# The colorgram module is a third-party library in Python that can be used
+# to extract colors from an image. It works by analyzing an image file and
+# creating a list of the dominant colors present in the image, along with
+# their RGB values. This can be useful in a variety of applications, such
+# as generating color palettes or analyzing the color composition of an
+# image. In the code provided, the colorgram module is used to extract a
+# list of 50 dominant colors from the image1.jpg file, which are then used
+# to randomly color the dots in the turtle graphic.
+# """
+# import colorgram
+#
+# """extracts and stores 50 colors from the provided image"""
+# colors = colorgram.extract("image1.jpg", 50)
+#
+# """rgbColors list"""
+# colorList = []
+#
+# """loop runs and appends all the colors as rgb value in rgbColors list"""
+# for color in colors:
+#     red = color.rgb.r
+#     green = color.rgb.g
+#     blue = color.rgb.b
+#     rgbColor = (red, green, blue)
+#     colorList.append(rgbColor)
+#
+# # tim's configurations
+# turtle.colormode(255)
+# tim.penup()
+# tim.speed("fastest")
+# tim.goto(-200, -200)
+#
+# dotsNumber = 100
+#
+# for dot in range(1, dotsNumber + 1):
+#     tim.dot(20, random.choice(colorList))
+#     tim.forward(40)
+#
+#     if dot % 10 == 0:
+#         tim.setheading(90)
+#         tim.forward(40)
+#         tim.setheading(180)
+#         tim.forward(400)
+#         tim.setheading(0)
+#
+# tim.hideturtle()
 
 """screen will be exited upon a click"""
 screen.exitonclick()
